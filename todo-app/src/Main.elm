5d1d8b7b9f8017ccf16cb1bf72d47f768399da29
Ashwin <aswinjeganath@gmail.com>
2020-04-14 07:44:20 +0530

    set up parcel + basic styling

--- todo-app/src/Main.elm
+++ todo-app/src/Main.elm
@@ -1,9 +1,9 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, text, ul, li)
-import Html.Events exposing (onClick, onInput)
+import Html exposing (Html, button, div, h1, li, text, ul)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
 import List
 
 
@@ -11,23 +11,38 @@ import List
 -- MAIN
 
 
+main : Program () Model Msg
 main =
-    Browser.sandbox { init = init, update = update, view = view }
+    Browser.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
 
 
 
 -- MODEL
-type alias Todo = { id: Int, action: String, completed: Bool }
+
+
+type alias Todo =
+    { id : Int
+    , action : String
+    , completed : Bool
+    }
+
 
 type alias Model =
     List Todo
 
 
-init : Model
-init =
-    [ (Todo 0 "Learn Elm" True)
-    , (Todo 1 "Take over the world" False)
-    ]
+init : () -> ( Model, Cmd msg )
+init _ =
+    ( [ Todo 0 "Learn Elm" True
+      , Todo 1 "Take over the world" False
+      ]
+    , Cmd.none
+    )
 
 
 
@@ -38,11 +53,11 @@ type Msg
     = Show
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd msg )
 update msg model =
     case msg of
         Show ->
-            model
+            ( model, Cmd.none )
 
 
 
@@ -51,11 +66,34 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div [] [ ul [] (List.map viewTodo model) ]
+    div [ class "container" ]
+        [ h1 [ class "heading" ] [ text "elm todo" ]
+        , viewTodoList model
+        ]
 
-viewTodo: Todo -> Html msg
+
+viewTodoList : List Todo -> Html msg
+viewTodoList todos =
+    ul [ class "todo-list" ] (List.map viewTodo todos)
+
+
+viewTodo : Todo -> Html msg
 viewTodo todo =
-    if todo.completed then
-        li [ style "text-decoration" "line-through" ] [ text todo.action ]
-    else
-        li [] [ text todo.action ]
+    let
+        className =
+            if todo.completed then
+                "todo-item complete"
+
+            else
+                "todo-item"
+    in
+    li [ class className ] [ Html.span [] [ text todo.action ] ]
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
